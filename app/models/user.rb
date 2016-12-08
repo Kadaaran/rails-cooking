c1d5126773b4d964f3471a6e8e990c0ae51d36da
Kassandre <kpedro@student.42.fr>
2016-12-08 02:23:29 +0100

    button unavaliable while required field not ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true,
-                    length: { minimum: 2 }
+                    length: { minimum: 4 }
   validates :email, presence: true,
-                    length: { minimum: 5 }
+                    length: { minimum: 4 }
 end
