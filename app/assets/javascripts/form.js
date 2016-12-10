be95e0e446542efadc38474ed557f4921c7c323b
Kassandre <kpedro@student.42.fr>
2016-12-10 22:08:35 +0100

    change css for button, button is disabled while the form's not valid

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -2,6 +2,8 @@ $(function() {
   $('form input').keyup(function(){
     var userLength = $('input#user_name').val().length;
     var emailLength = $('input#user_email').val().length;
-    $('.button').prop('disabled', userLength < 4 || emailLength < 4);
+    console.log(userLength, emailLength);
+    console.log(userLength < 4 || emailLength < 4);
+    $('.button input').prop('disabled', userLength < 4 || emailLength < 4);
   });
 });
